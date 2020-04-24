03b8e59f4f0b61f933c05e330d130a017f4cde2c
Mihai Hudisteanu <hudisteanum@yahoo.com>
2020-04-24 18:29:19 +0300

    Tests are now in methods (Main)

--- whistGame/Model/src/main/java/domain/game/main.java
+++ whistGame/Model/src/main/java/domain/game/main.java
@@ -2,12 +2,19 @@ package domain.game;
 
 import java.util.List;
 
+
+
 public class main {
-    public static void main(String[] args) {
+
+    public static void deckTest(){
         Deck deck = new Deck(16);
         List<Card> cards = deck.generateCards();
         for (Card card: cards){
             System.out.println(card.getValue() + " " + card.getSymbol());
         }
+    }
+
+    public static void main(String[] args) {
+      deckTest();
     }
 }
