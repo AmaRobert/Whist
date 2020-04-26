a8d9ab3dceeb531bc3242668643783724ad2449f
Mihai Hudisteanu <hudisteanum@yahoo.com>
2020-04-26 18:40:30 +0300

    small changes, packages mostly

--- whistGame/Model/src/main/java/domain/game/main.java
+++ whistGame/Model/src/main/java/domain/game/main.java
@@ -1,24 +1,24 @@
 
 package domain.game;
 
-import java.util.List;
 
+import java.util.List;
 
 
 public class main {
 
-    public static void deckTest(){
+//    public static void deckTest(){
+//
+//        domain.Game.Deck deck = new domain.Game.Deck(16);
+//        List<Card> cards = deck.generateCards();
+//        for (Card card: cards){
+//            System.out.println(card.getValue() + " " + card.getSymbol());
+//        }
+//    }
 
-        Deck deck = new Deck(16);
-        List<Card> cards = deck.generateCards();
-        for (Card card: cards){
-            System.out.println(card.getValue() + " " + card.getSymbol());
-        }
-    }
 
-
-    public static void main(String[] args) {
-      deckTest();
-    }
+//    public static void main(String[] args) {
+//      deckTest();
+//    }
 
 }
